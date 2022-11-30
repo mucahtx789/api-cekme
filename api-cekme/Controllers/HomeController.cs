2f608cf90b86debb0b96359828fd23be919bc0f6
mucahtx789 <76493181+mucahtx789@users.noreply.github.com>
2022-11-30 17:08:23 +0300

    a

---
+++ api-cekme/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using api_cekme.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace api_cekme.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+
+            // DateTime now = DateTime.Now;
+            string apiurl = "https://api.genelpara.com/embed/para-birimleri.json";
+
+            WebClient client = new WebClient();
+            var json = client.DownloadString(apiurl);
+
+          
+           
+             kurmodel a = JsonConvert.DeserializeObject<kurmodel>(json);
+            string usd = a.USD.alis;
+            
+            //ViewData["usd"] = usd;
+
+            return View(a);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ api-cekme/Models/kurmodel.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace api_cekme.Models
+{
+   public class info
+    {
+        public string satis { get; set; }
+        public string alis { get; set; }
+        public string degisim { get; set; }
+
+    }
+    
+    public class kurmodel
+    {
+        //  public info kurlar { get; set; }
+        public info USD { get; set; }
+        public info EUR { get; set; }
+        public info GBP { get; set; }
+        public info BTC { get; set; }
+        public info ETH { get; set; }
+        public info GA { get; set; }
+        public info C { get; set; }
+        public info GAG { get; set; }
+        public info XU100 { get; set; }
+    }
+    
+}
